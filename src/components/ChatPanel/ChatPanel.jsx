7a9b7853ef616b4f1923103162ea5cbc262edb90
Dan Vasudevan <danvasudevan@Dans-MacBook-Air-2.local>
2025-01-28 13:57:30 +0530

    chat history changes

--- src/components/ChatPanel/ChatPanel.jsx
+++ src/components/ChatPanel/ChatPanel.jsx
@@ -1,12 +1,12 @@
 import './ChatPanel.css';
 import FileInputPanel from './FileInputPanel/FileInputPanel';
-import ConversationHistory from './ConversationHistory/ConversationHistory';
+import ChatHistory from './ChatHistory/ChatHistory';
 
 function ChatPanel() {
   return (
     <>
       <div className="ChatPanel">
-        <ConversationHistory />
+        <ChatHistory />
         <div className="FileInput">
           <FileInputPanel />
         </div>
